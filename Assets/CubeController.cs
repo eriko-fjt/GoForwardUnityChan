94caf618ea8fbb740879d7104ab706d6af841a14
eriko-fjt <tangtin.waileizi@gmail.com>
2020-07-07 23:51:13 +0900

    GoForwardUnityChan  課題提出用１回目コミット
    キューブの接触時に効果音をつけた

---
+++ Assets/CubeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	private float speed = -12;
+
+
+	private float deadLine = -10;
+
+
+	// 課題
+	
+
+	public AudioClip block;
+	AudioSource audioSource;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		audioSource = GetComponent<AudioSource>();
+		
+	}
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+
+		transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+
+		if (transform.position.x < this.deadLine)
+		{
+			Destroy(gameObject);
+		}
+
+	}
+
+	public void OnCollisionEnter2D(Collision2D collision)
+	{
+		
+		if (collision.gameObject.tag == "CubeTag" || collision.gameObject.tag == "GroundTag")
+		{
+			audioSource.PlayOneShot(block, 1.0f);
+			
+		}
+	}
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+
+	Animator animator;
+
+	Rigidbody2D rigid2D;
+
+	private float groundLevel = -3.0f;
+
+	private float dump = 0.8f;
+
+	float jumpVelocity = 20;
+
+
+	private float deadLine = -9;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		this.animator = GetComponent<Animator> ();
+
+		this.rigid2D = GetComponent<Rigidbody2D>();
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.animator.SetFloat("Horizontal", 1);
+
+		bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+
+		// 着地しているかどうかを調べる
+		this.animator.SetBool("isGround", isGround);
+
+
+		GetComponent<AudioSource>().volume = (isGround) ? 1 : 0;
+
+
+		// 着地状態でクリックされた場合
+		if (Input.GetMouseButtonDown (0) && isGround)
+        {
+			// 上方向の力をかける
+			this.rigid2D.velocity = new Vector2(0, this.jumpVelocity);
+        }
+
+
+		// クリックをやめたら上方向への速度を減速する
+		if (Input.GetMouseButton (0) == false)
+        {
+			if (this.rigid2D.velocity.y > 0)
+            {
+				this.rigid2D.velocity *= this.dump;
+            }
+        }
+
+
+
+		if (transform.position.x < this.deadLine)
+        {
+			GameObject.Find("Canvas").GetComponent<UIController>().GameOver();
+
+			Destroy(gameObject);
+        }
+		
+	}
+}
